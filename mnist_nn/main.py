b79821bc71a55d5e5255eeeb1221c4bca7560339
Marcus Nilszén <nilszen.marcus@gmail.com>
2023-12-14 12:09:58 +0100

    mnist and pytorch

--- mnist_nn/main.py
+++ mnist_nn/main.py
@@ -1,8 +1,13 @@
 import torch
 import torchvision
 from torchvision import transforms, datasets
+import matplotlib.pyplot as plt
 
 # data
 train = datasets.MNIST("../data", train=True, download=True, transform=transforms.ToTensor())
 test = datasets.MNIST("../data", train=False, download=True, transform=transforms.ToTensor())
+
+# set
+trainset = torch.utils.data.DataLoader(train, batch_size=64, shuffle=True)
+testset = torch.utils.data.DataLoader(test, batch_size=64, shuffle=True)
 
